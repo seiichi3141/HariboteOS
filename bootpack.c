a230822e3ef6b6414a08af0356f6f010d78eb2ea
Seiichiro Tanaka <seiichiro_tanaka@toshibatec.co.jp>
2017-04-14 17:13:43 +0900

    harib01g 途中

--- bootpack.c
+++ bootpack.c
@@ -7,12 +7,31 @@ void io_store_eflags(int eflags);
 
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
+
 
 void HariMain(void) {
 	int i;
 
 	init_palette();
-	
+
 	for (i = 0xa0000; i <= 0xaffff; i++) {
 		*((char*)i) = i & 0x0f;
 	}
