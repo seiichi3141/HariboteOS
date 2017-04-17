562ad12e5eed6811f93045d0fc674b11628082de
Seiichiro Tanaka <seiichiro_tanaka@toshibatec.co.jp>
2017-04-17 17:04:30 +0900

    harib05a

--- bootpack.c
+++ bootpack.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "bootpack.h"
 
-extern struct FIFO8 keyfifo;
+extern struct FIFO8 keyfifo, mousefifo;
 void enable_mouse(void);
 void init_keyboard(void);
 
@@ -12,8 +12,9 @@ void HariMain(void) {
 	init_pic();
 	io_sti();
 	
-	char keybuf[32];
+	char keybuf[32], mousebuf[128];
 	fifo8_init(&keyfifo, 32, keybuf);
+	fifo8_init(&mousefifo, 128, mousebuf);
 
 	io_out8(PIC0_IMR, 0xf9);
 	io_out8(PIC1_IMR, 0xef);
@@ -35,17 +36,40 @@ void HariMain(void) {
 
 	enable_mouse();
 	
+	unsigned char mouse_dbuf[3], mouse_phase = 0;
 	for (;;) {
 		io_cli();
-		if (fifo8_status(&keyfifo) == 0) {
-			io_stihlt();
-		} else {
+		if (fifo8_status(&keyfifo) != 0) {
 			int i = fifo8_get(&keyfifo);
 			io_sti();
 			unsigned char s[40];
 			sprintf(s, "%02X", i);
 			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
 			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+		} else if (fifo8_status(&mousefifo) != 0) {
+			int i = fifo8_get(&mousefifo);
+			io_sti();
+			if (mouse_phase == 0) {
+				if (i == 0xfa) {
+					mouse_phase = 1;
+				}
+			} else if (mouse_phase == 1) {
+				mouse_dbuf[0] = i;
+				mouse_phase = 2;
+			} else if (mouse_phase == 2) {
+				mouse_dbuf[1] = i;
+				mouse_phase = 3;
+			} else if (mouse_phase == 3) {
+				mouse_dbuf[2] = i;
+				mouse_phase = 1;
+
+				unsigned char s[40];
+				sprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 * 8 * 8 - 1, 31);
+				putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+			}
+		} else {
+			io_stihlt();
 		}
 	}
 }
