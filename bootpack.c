7bfc4d0f98a67596fd62c6ed30d65bcbfbbdf192
Seiichirou Tanaka <Seiichirou Tanaka>
2017-04-18 22:12:18 +0900

    harib06b

--- bootpack.c
+++ bootpack.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "bootpack.h"
 
+unsigned int memtest(unsigned int start, unsigned int end);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
+
 void HariMain(void) {
 	struct BOOTINFO *binfo = (struct BOOTINFO*)ADR_BOOTINFO;
 
@@ -32,6 +35,10 @@ void HariMain(void) {
 
 	struct MOUSE_DEC mdec;
 	enable_mouse(&mdec);
+
+	int i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
+	sprintf(s, "memory %dMB", i);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 	
 	for (;;) {
 		io_cli();
@@ -84,4 +91,59 @@ void HariMain(void) {
 			io_stihlt();
 		}
 	}
+}
+
+#define EFLAGS_AC_BIT		0x00040000
+#define CR0_CACHE_DISABLE	0x60000000
+
+unsigned int memtest(unsigned int start, unsigned int end) {
+	char flg486 = 0;
+	unsigned int eflg, cr0, i;
+
+	eflg = io_load_eflags();
+	eflg |= EFLAGS_AC_BIT;
+	io_store_eflags(eflg);
+	eflg = io_load_eflags();
+	if ((eflg & EFLAGS_AC_BIT) != 0) {
+		flg486 = 1;
+	}
+	eflg &= ~EFLAGS_AC_BIT;
+	io_store_eflags(eflg);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 |= CR0_CACHE_DISABLE;
+		store_cr0(cr0);
+	}
+
+	i = memtest_sub(start, end);
+
+	if (flg486 != 0) {
+		cr0 = load_cr0();
+		cr0 &= ~CR0_CACHE_DISABLE;
+		store_cr0(cr0);
+	}
+
+	return i;
+}
+
+unsigned int memtest_sub(unsigned int start, unsigned int end) {
+	unsigned int i, *p, old, pat0 = 0xaa55aa55, pat1 = 0x55aa5aa;
+	for (i = start; i <= end; i += 0x1000) {
+		p = (unsigned int*)(i + 0xffc);
+		old = *p;
+		*p = pat0;
+		*p ^= 0xffffffff;
+		if (*p != pat1) {
+not_memory:
+			*p = old;
+			break;
+		}
+		*p ^= 0xffffffff;
+		if (*p != pat0) {
+			goto not_memory;
+		}
+		*p = old;
+	}
+	return i;
 }
