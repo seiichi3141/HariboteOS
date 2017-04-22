a69e467efd97c9b3c7ac772ca4a0e33d14710446
Seiichirou Tanaka <Seiichirou Tanaka>
2017-04-22 17:22:51 +0900

    harib07c

--- bootpack.c
+++ bootpack.c
@@ -53,7 +53,7 @@ void HariMain(void) {
 		memtotal / (1024 * 1024), memman_total(memman) / 1024);;
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 	
-	sheet_refresh(shtctl);
+	sheet_refresh(shtctl, sht_back, 0, 0, binfo->scrnx, 48);
 
 	for (;;) {
 		io_cli();
@@ -64,7 +64,7 @@ void HariMain(void) {
 			sprintf(s, "%02X", i);
 			boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
 			putfonts8_asc(buf_back, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-			sheet_refresh(shtctl);
+			sheet_refresh(shtctl, sht_back, 0, 16, 16, 32);
 		} else if (fifo8_status(&mousefifo) != 0) {
 			int i = fifo8_get(&mousefifo);
 			io_sti();
@@ -82,8 +82,8 @@ void HariMain(void) {
 				}
 				boxfill8(buf_back, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
 				putfonts8_asc(buf_back, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+				sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15 * 8, 32);
 
-				//boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15);
 				mx += mdec.x;
 				my += mdec.y;
 				if (mx < 0) {
@@ -101,7 +101,7 @@ void HariMain(void) {
 				sprintf(s, "(%3d, %3d)", mx, my);
 				boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
 				putfonts8_asc(buf_back, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
-				//putblock8_8(buf_back, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+				sheet_refresh(shtctl, sht_back, 0, 0, 80, 16);
 				sheet_slide(shtctl, sht_mouse, mx, my);
 			}
 		} else {
