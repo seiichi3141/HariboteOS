ec96affe269d982175affbbe1867ecf622959d13
Seiichirou Tanaka <Seiichirou Tanaka>
2017-04-26 23:14:59 +0900

    harib09g

--- timer.c
+++ timer.c
@@ -14,9 +14,10 @@ void init_pit(void) {
 	io_out8(PIT_CNT0, 0x2e);
 	timerctl.count = 0;
 	timerctl.next = 0xffffffff;
+	timerctl.using = 0;
 	int i;
 	for (i = 0; i < MAX_TIMER; i++) {
-		timerctl.timer[i].flags = 0;
+		timerctl.timers0[i].flags = 0;
 	}
 	return;
 }
@@ -24,9 +25,9 @@ void init_pit(void) {
 struct TIMER *timer_alloc(void) {
 	int i;
 	for (i = 0; i < MAX_TIMER; i++) {
-		if (timerctl.timer[i].flags == 0) {
-			timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
-			return &timerctl.timer[i];
+		if (timerctl.timers0[i].flags == 0) {
+			timerctl.timers0[i].flags = TIMER_FLAGS_ALLOC;
+			return &timerctl.timers0[i];
 		}
 	}
 	return 0;
@@ -46,9 +47,21 @@ void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data) {
 void timer_settime(struct TIMER *timer, unsigned int timeout) {
 	timer->timeout = timeout + timerctl.count;
 	timer->flags = TIMER_FLAGS_USING;
-	if (timerctl.next > timer->timeout) {
-		timerctl.next = timer->timeout;
+	int e = io_load_eflags();
+	io_cli();
+	int i, j;
+	for (i = 0; i < timerctl.using; i++) {
+		if (timerctl.timers[i]->timeout >= timer->timeout) {
+			break;
+		}
 	}
+	for (j = timerctl.using; j > i; j--) {
+		timerctl.timers[j] = timerctl.timers[j - 1];
+	}
+	timerctl.using++;
+	timerctl.timers[i] = timer;
+	timerctl.next = timerctl.timers[0]->timeout;
+	io_store_eflags(e);
 	return;
 }
 
@@ -58,19 +71,22 @@ void inthandler20(int *esp) {
 	if (timerctl.next > timerctl.count) {
 		return;
 	}
-	timerctl.next = 0xffffffff;
-	int i;
-	for (i = 0; i < MAX_TIMER; i++) {
-		if (timerctl.timer[i].flags == TIMER_FLAGS_USING) {
-			if (timerctl.timer[i].timeout <= timerctl.count) {
-				timerctl.timer[i].flags = TIMER_FLAGS_ALLOC;
-				fifo8_put(timerctl.timer[i].fifo, timerctl.timer[i].data);
-			} else {
-				if (timerctl.next > timerctl.timer[i].timeout) {
-					timerctl.next = timerctl.timer[i].timeout;
-				}
-			}
+	int i, j;
+	for (i = 0; i < timerctl.using; i++) {
+		if (timerctl.timers[i]->timeout > timerctl.count) {
+			break;
 		}
+		timerctl.timers[i]->flags = TIMER_FLAGS_ALLOC;
+		fifo8_put(timerctl.timers[i]->fifo, timerctl.timers[i]->data);
+	}
+	timerctl.using -= i;
+	for (j = 0; j < timerctl.using; j++) {
+		timerctl.timers[j] = timerctl.timers[i + j];
+	}
+	if (timerctl.using > 0) {
+		timerctl.next = timerctl.timers[0]->timeout;
+	} else {
+		timerctl.next = 0xffffffff;
 	}
 	return;
 }
