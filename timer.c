13bc32c8f4ce6836fac0774e0126d5f86f115a69
Seiichirou Tanaka <Seiichirou Tanaka>
2017-04-30 16:14:37 +0900

    harib10h

--- timer.c
+++ timer.c
@@ -49,19 +49,41 @@ void timer_settime(struct TIMER *timer, unsigned int timeout) {
 	timer->flags = TIMER_FLAGS_USING;
 	int e = io_load_eflags();
 	io_cli();
-	int i, j;
-	for (i = 0; i < timerctl.using; i++) {
-		if (timerctl.timers[i]->timeout >= timer->timeout) {
+	timerctl.using++;
+	if (timerctl.using == 1) {
+		timerctl.t0 = timer;
+		timer->next = 0;
+		timerctl.next = timer->timeout;
+		io_store_eflags(e);
+		return;
+	}
+	struct TIMER *t = timerctl.t0;
+	if (timer->timeout <= t->timeout) {
+		timerctl.t0 = timer;
+		timer->next = t;
+		timerctl.next = timer->timeout;
+		io_store_eflags(e);
+		return;
+	}
+	struct TIMER *s;
+	for (;;) {
+		s = t;
+		t = t->next;
+		if (t == 0) {
 			break;
 		}
+		if (timer->timeout <= t->timeout) {
+			s->next = timer;
+			timer->next = t;
+			io_store_eflags(e);
+			return;
+		}
 	}
-	for (j = timerctl.using; j > i; j--) {
-		timerctl.timers[j] = timerctl.timers[j - 1];
-	}
-	timerctl.using++;
-	timerctl.timers[i] = timer;
-	timerctl.next = timerctl.timers[0]->timeout;
+
+	s->next = timer;
+	timer->next = 0;
 	io_store_eflags(e);
+
 	return;
 }
 
@@ -71,20 +93,22 @@ void inthandler20(int *esp) {
 	if (timerctl.next > timerctl.count) {
 		return;
 	}
-	int i, j;
+	int i;
+	struct TIMER *timer = timerctl.t0;
 	for (i = 0; i < timerctl.using; i++) {
-		if (timerctl.timers[i]->timeout > timerctl.count) {
+		if (timer->timeout > timerctl.count) {
 			break;
 		}
-		timerctl.timers[i]->flags = TIMER_FLAGS_ALLOC;
-		fifo32_put(timerctl.timers[i]->fifo, timerctl.timers[i]->data);
+		timer->flags = TIMER_FLAGS_ALLOC;
+		fifo32_put(timer->fifo, timer->data);
+		timer = timer->next;
 	}
+
 	timerctl.using -= i;
-	for (j = 0; j < timerctl.using; j++) {
-		timerctl.timers[j] = timerctl.timers[i + j];
-	}
+	timerctl.t0 = timer;
+
 	if (timerctl.using > 0) {
-		timerctl.next = timerctl.timers[0]->timeout;
+		timerctl.next = timerctl.t0->timeout;
 	} else {
 		timerctl.next = 0xffffffff;
 	}
