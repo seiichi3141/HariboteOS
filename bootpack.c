65a95dbe7a3f6cab783d9e5186b574468c318cdb
Seiichiro Tanaka <seiichiro_tanaka@toshibatec.co.jp>
2017-05-10 17:21:01 +0900

    harib15b

--- bootpack.c
+++ bootpack.c
@@ -193,11 +193,13 @@ void HariMain(void) {
 						make_wtitle8(buf_cons, sht_cons->bxsize, "console", 1);
 						cursor_c = -1;
 						boxfill8(sht_win->buf, sht_win->bxsize, COL8_FFFFFF, cursor_x, 28, cursor_x + 7, 43);
+						fifo32_put(&task_cons->fifo, 2);	/* コンソールのカーソル有効 */
 					} else {
 						key_to = 0;
 						make_wtitle8(buf_win, sht_win->bxsize, "task_a", 1);
 						make_wtitle8(buf_cons, sht_cons->bxsize, "console", 0);
 						cursor_c = COL8_000000;
+						fifo32_put(&task_cons->fifo, 3);	/* コンソールのカーソル無効 */
 					}
 					sheet_refresh(sht_win, 0, 0, sht_win->bxsize, 21);
 					sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
@@ -380,7 +382,7 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c) {
 void console_task(struct SHEET *sheet) {
 	struct TIMER *timer;
 	struct TASK *task = task_now();
-	int i, fifobuf[128], cursor_x = 16, cursor_c = COL8_000000;
+	int i, fifobuf[128], cursor_x = 16, cursor_c = -1;
 	char s[2];
 
 	fifo32_init(&task->fifo, 128, fifobuf, task);
@@ -402,12 +404,23 @@ void console_task(struct SHEET *sheet) {
 			if (i <= 1) {
 				if (i != 0) {
 					timer_init(timer, &task->fifo, 0);
-					cursor_c = COL8_FFFFFF;
+					if (cursor_c >= 0) {
+						cursor_c = COL8_FFFFFF;
+					}
 				} else {
 					timer_init(timer, &task->fifo, 1);
-					cursor_c = COL8_000000;
+					if (cursor_c >= 0) {
+						cursor_c = COL8_000000;
+					}
 				}
 				timer_settime(timer, 50);
+			}
+			if (i == 2) {	/* カーソル有効 */
+				cursor_c = COL8_FFFFFF;
+			}
+			if (i == 3) {	/* カーソル無効 */
+				boxfill8(sheet->buf, sheet->bxsize, COL8_000000, cursor_x, 28, cursor_x + 7, 43);
+				cursor_c = -1;
 			}
 			if (256 <= i && i <= 511) {
 				if (i == 0 + 256) {
@@ -424,7 +437,9 @@ void console_task(struct SHEET *sheet) {
 					}
 				}
 			}
-			boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+			if (cursor_c >= 0) {
+				boxfill8(sheet->buf, sheet->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
+			}			
 			sheet_refresh(sheet, cursor_x, 28, cursor_x + 8, 44);
 		}
 	}
